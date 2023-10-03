1cffdcc21d25370adfb59b0a68c14ee9110b3afc
Brewer <115757514+BrewerDeveloping@users.noreply.github.com>
2023-10-03 20:31:53 +0000

    Revert

--- brewingweather/frontend/brewingweather/src/components/LandingPage.jsx
+++ brewingweather/frontend/brewingweather/src/components/LandingPage.jsx
@@ -3,8 +3,7 @@ import "../css/landingpage.css";
 function HelloMessage() {
   return (
     <div className="welcome">
-      <h1>THIS APPLICATION IS CURRENTLY UNDER CONSTRUCTION -- IF YOU FOUND THIS IN A LIVE SETTING PLEASE BE 
-        AWARE MANY CHANGES WILL BE OCCURING IN THE MONTHS TO COME.</h1>
+      <h1>Welcome to the Weather App you didn't know you needed!</h1>
     </div>
   );
 }
