8fa3e1a9469fd9b2db8269b4451ed83ca4a651d3
Brewer <115757514+BrewerDeveloping@users.noreply.github.com>
2023-10-03 20:17:17 +0000

    Updated LandingPage to indicate construction

--- brewingweather/frontend/brewingweather/src/components/LandingPage.jsx
+++ brewingweather/frontend/brewingweather/src/components/LandingPage.jsx
@@ -3,7 +3,8 @@ import "../css/landingpage.css";
 function HelloMessage() {
   return (
     <div className="welcome">
-      <h1>Welcome to the Weather App you didn't know you needed!</h1>
+      <h1>THIS APPLICATION IS CURRENTLY UNDER CONSTRUCTION -- IF YOU FOUND THIS IN A LIVE SETTING PLEASE BE 
+        AWARE MANY CHANGES WILL BE OCCURING IN THE MONTHS TO COME.</h1>
     </div>
   );
 }
