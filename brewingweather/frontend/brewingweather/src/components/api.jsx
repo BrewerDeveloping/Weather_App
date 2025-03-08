0e8a44b76cb2d6633a9e88451aea5129c120f209
Brewer <115757514+BrewerDeveloping@users.noreply.github.com>
2025-03-08 00:59:43 -0600

    Update api.jsx

--- brewingweather/frontend/brewingweather/src/components/api.jsx
+++ brewingweather/frontend/brewingweather/src/components/api.jsx
@@ -8,4 +8,4 @@ export const geoApiOptions = {
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/3.0";
-export const WEATHER_API_KEY = "9c8a5c95360d235c98b56049ae4102e7";
+export const WEATHER_API_KEY = "YOUR_API_KEY";
